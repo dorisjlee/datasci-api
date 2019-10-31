6b6510ec21b47fcf17b904335d54f686836a0695
dorisjlee <dorisjunglinlee@gmail.com>
2019-10-31 12:35:27 -0700

    basic data object functionalities and test datasets

---
+++ dataMgr/Data.py
@@ -0,0 +1,39 @@
+import pandas as pd
+
+class Data:
+	def __init__(self,filename):
+		self.filename = filename
+		self.df = self.loadCSV()
+		self.dataType = self.getDataType()
+		self.dataModel = self.getDataModel()
+		self.computeStats()
+	def __repr__(self):
+		return f"<Data Obj: {str(self.filename)}>"
+
+	def loadCSV(self):
+		df = pd.read_csv(self.filename)
+		return df
+
+	def getDataType(self):
+		df = self.df
+		dataType = {
+			"quantitative":list(df.dtypes[df.dtypes=="float64"].keys()),
+			"categorical":list(df.dtypes[df.dtypes=="object"].keys()),
+			"ordinal":list(df.dtypes[df.dtypes=="int64"].keys())
+		}
+		return dataType
+	def getDataModel(self):
+		dataModel = {
+			"measure":self.dataType["quantitative"],
+			"dimension":self.dataType["ordinal"]+self.dataType["categorical"]
+		}
+		return dataModel
+	def computeStats(self):
+		# precompute statistics
+		def cardinality(df,columnName):
+			return len(df[columnName].unique())
+		self.cardinality = {}
+		for dimension in self.dataModel["dimension"]:
+			self.cardinality[dimension]=cardinality(self.df,dimension)
+		
+		
