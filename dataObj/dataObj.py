a245979c22fc22e98ae1ebf2c70249260b5bf99d
dorisjlee <dorisjunglinlee@gmail.com>
2019-11-16 10:16:37 -0800

    migrated autoencoding code to be separate from DataObj

--- dataObj/dataObj.py
+++ dataObj/dataObj.py
@@ -1,5 +1,6 @@
 from dataObj.Row import Row
 from dataObj.Column import Column
+from vizLib.Autoencoding import Autoencoding
 class DataObj:
 	'''
 	DataObj is an abstract object representing some aspect of the data. 
@@ -13,8 +14,16 @@ class DataObj:
 		self.expandUnderspecified()
 
 	def __repr__(self):
+		# TODO: figure out a way to call display when printing out a data obj
+		# currently repr can not be used for printing out non-string values. (Ref to how Dataframe is displayed by default in Pandas)
 		return f"<Data Obj: {str(self.dataset)} -- {str(self.spec)}>"
 
+	# def __str__(self):
+	# 	vis = self.display()
+	# 	if (vis): 
+	# 		return vis
+	# 	else: 
+	# 		return f"<Data Obj: {str(self.dataset)} -- {str(self.spec)}>"
 	def expandUnderspecified(self):
 		for rcObj in self.spec:
 			if (rcObj.dataType==""):
@@ -23,71 +32,8 @@ class DataObj:
 			if (rcObj.dataModel==""):
 				rcObj.dataModel = self.dataset.dataModelLookup[rcObj.columnName]
 			
-	def display(self): 
+	def display(self,renderer="altair"): 
 		# render this data object as: vis, columns, etc.?
-		return self.showMe()
-		# return NotImplemented
-	def showMe(self):
-		# TODO: possibly implement chart alternatives as a list of possible encodings
-		dobj = self 
-		# TODO: Need to generalize this import to other rendering mechanisms
-		from vizLib.altair.BarChart import BarChart
-		from vizLib.altair.ScatterChart import ScatterChart
-		from vizLib.altair.Histogram import Histogram
-		from vizLib.altair.LineChart import LineChart
-		# Count number of measures and dimensions
-		Ndim = 0 
-		Nmsr = 0 
-		for spec in dobj.spec: 
-			if (spec.dataModel == "dimension"):
-				Ndim +=1
-			elif (spec.dataModel == "measure"):
-				Nmsr +=1
-		print ("Ndim,Nmsr:",Ndim,Nmsr)
-		# Helper function (TODO: Move this into utils)
-		def lineOrBar(dobj):
-			dimension = filterDataModel(dobj,"dimension")[0]
-			dimType = dimension.dataType
-			if (dimType =="date" or dimType == "oridinal"):
-				chart = LineChart(dobj)
-			else: # unordered categorical
-				chart = BarChart(dobj)
-				# TODO: if cardinality large than 6 then sort bars
-			return chart
-		def filterDataModel(dobj,dmodel):       
-			return list(filter(lambda x: x.dataModel==dmodel if hasattr(x,"dataModel") else False,dobj.spec))
-		# ShowMe logic + additional heuristics
-		countCol = Column("Count")
-		if (Ndim == 0 and Nmsr ==1):
-			# Histogram with Count on the y axis
-			chart = Histogram(dobj)
-		elif (Ndim ==2 and Nmsr==0):
-			pass
-		elif (Ndim ==0 and Nmsr==2):
-			# Scatterplot
-			chart = ScatterChart(dobj)
-		elif (Ndim ==1 and Nmsr ==2):
-			# Scatterplot broken down by the dimension
-			pass 
-		elif (Ndim ==1 and (Nmsr ==0 or Nmsr==1)):
-			# Bar Chart
-			if (Nmsr==0): dobj.spec.append(countCol)
-			chart = lineOrBar(dobj)
-		elif (Ndim ==2 and Nmsr==1):
-			print("here")
-			# Bar/Line chart broken down by dimension
-			dimension = filterDataModel(dobj,"dimension")
-			d1 = dimension[0]
-			d2 = dimension[1]
-			if (dobj.dataset.cardinality[d1.columnName]<dobj.dataset.cardinality[d2.columnName]):
-				d1.channel = "color"
-			else:
-				d2.channel = "color"
-			chart = lineOrBar(dobj)
-			# TODO: Generalize to breakdown by? 
-			chart.chart = chart.chart.encode(color="Origin")
-		elif (Ndim==3):
-			pass
-		else:
-			pass
-		return chart.chart
+		encoder = Autoencoding(renderer)
+		return encoder.createVis(self)
+	
