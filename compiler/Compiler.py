82eae6618044e1e6d48b166f66cbd7bf7a260fd3
jaywoo123 <jkim475@illinois.edu>
2019-11-26 03:35:00 -0600

    adding z axis iteration

--- compiler/Compiler.py
+++ compiler/Compiler.py
@@ -16,6 +16,10 @@ class Compiler:
 				if (rcObj.dataModel==""):
 					rcObj.dataModel = dobj.dataset.dataModelLookup[rcObj.columnName]
 
+	def applyDataTransformations(self,dataset,fAttribute,fVals):
+		if fVals[0] != "?":
+			dataset.df = dataset.df[dataset.df[fAttribute].isin(fVals)]
+		return dataset
 
 	def enumerateCollection(self,dobj):
 		from dataObj.dataObj import DataObj
@@ -35,27 +39,25 @@ class Compiler:
 		rowSpecs = list(filter(lambda x: x.className=="Row", dobj.spec))
 		col1Attrs = []
 		col2Attrs = []
-		rowAttrs = []
 		# TODO: This needs to be rewritten in a recursive manner so that the channel and other specification can be inheritted
 		if len(colSpecs)>0:
 			col1Attrs = convert2List(colSpecs[0].columnName)
 		if len(colSpecs)>1:
 			col2Attrs = convert2List(colSpecs[1].columnName)
 		if len(rowSpecs)>0:
-			rowAttrs = convert2List(rowSpecs[0].rowName)
+			rowVals = convert2List(rowSpecs[0].fVal)
 		# Generate Collection
-
 		collection = []
 		if (len(col1Attrs)<=1 and len(col2Attrs)<=1 and len(rowSpecs)<=1):
 			return  DataObjCollection(collection)
 		else:
 			for col1 in col1Attrs:
 				for col2 in col2Attrs:
-					if len(rowAttrs)>0:
-						for row in rowAttrs:
-							# create the data objects
-							dataObj = DataObj(dobj.dataset,[Column(col1), Column(col2), Row(fAttribute=row,fVal='?')])
-							collection.append(dataObj)
+					if len(rowVals)>0:
+						# create the data objects
+						transformedDataset = self.applyDataTransformations(dobj.dataset, rowSpecs[0].fAttribute,rowVals) #rename?
+						dataObj = DataObj(transformedDataset,[Column(col1), Column(col2)])
+						collection.append(dataObj)
 					else:
 						dataObj = DataObj(dobj.dataset, [Column(col1), Column(col2)])
 						collection.append(dataObj)
