26d258021b0b1e88519be2052344ef7e0416a7e7
Doris Lee <dorisjunglinlee@gmail.com>
2019-12-13 10:34:12 -0800

    bug fix

--- dataObj/Row.py
+++ dataObj/Row.py
@@ -2,7 +2,7 @@ class Row:
 	'''
 	Row Object represents one or a group of datapoints (rows) in the Dataset.
 	'''
-	def __init__(self, fAttribute="",fOp = "=", fVal= "", function = "filter"):
+	def __init__(self, fAttribute="", fVal= "",fOp = "=", function = "filter"):
 		self.className = "Row"
 		# Constraint-based specification (e.g., Origin = USA) #TODO: need to extend this with logical operators
 		self.fAttribute = fAttribute
