bug fix for correlation calculation

action/Correlation.py
@@ -16,6 +16,7 @@ def correlation(dobj,ignoreIdentity=True,ignoreTranspose=True):
 		else:
 			return False
 	for obj in vizCollection:
+		
 		measures = obj.getObjByDataModel("measure")
 		if len(measures)<2 : raise ValueError(f"Can not compute correlation between {[x.columnName for x in obj.spec]} since less than 2 measure values present.")
 		msr1 = measures[0].columnName
@@ -27,7 +28,8 @@ def correlation(dobj,ignoreIdentity=True,ignoreTranspose=True):
 			checkTranspose = checkTransposeNotComputed(dobj,msr1,msr2)
 		else:
 			checkTranspose = True
-		if (checkTranspose):
+
+		if (not checkTranspose):
 			obj.score = np.abs(scipy.stats.pearsonr(msr1Vals,msr2Vals)[0])
 		else:
 			obj.score = -1	