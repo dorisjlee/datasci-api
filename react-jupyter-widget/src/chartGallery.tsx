f3f9c6be136b3d44996a1aa8bb355281705ccc6e
Doris Lee <dorisjunglinlee@gmail.com>
2019-12-26 21:40:44 -0800

    fixed installation issue with webpack;prelminary vegalite embed code

--- react-jupyter-widget/src/chartGallery.tsx
+++ react-jupyter-widget/src/chartGallery.tsx
@@ -1,11 +1,33 @@
 import React, { Component } from 'react';
 import ToolComponent from './tool';
-// var vegaEmbed = require('vega-embed');
+var vegaEmbed = require('vega-embed');
 
 class ChartGalleryComponent extends Component {
     constructor(props:any) {
         super(props);
         // const numbers = props.numbers;
+    }
+    componentDidMount(){
+        // runs after the component output has been rendered to the DOM 
+        // var spec = JSON.parse(this.model.get('_graph_specs')[num]);
+        //     console.log(spec);
+        let spec = {
+            "$schema": "https://vega.github.io/schema/vega-lite/v4.json",
+            "description": "A simple bar chart with embedded data.",
+            "data": {
+              "values": [
+                {"a": "A", "b": 28}, {"a": "B", "b": 55}, {"a": "C", "b": 43},
+                {"a": "D", "b": 91}, {"a": "E", "b": 81}, {"a": "F", "b": 53},
+                {"a": "G", "b": 19}, {"a": "H", "b": 87}, {"a": "I", "b": 52}
+              ]
+            },
+            "mark": "bar",
+            "encoding": {
+              "x": {"field": "a", "type": "ordinal"},
+              "y": {"field": "b", "type": "quantitative"}
+            }
+          }
+        vegaEmbed.default("#graph-container-0", spec);
     }
     render() {
         var graphSpecs = [4,5,2,4,2]
