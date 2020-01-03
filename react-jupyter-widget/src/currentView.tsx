bad7373721737cbce886c4e981e4e606ed4798cf
Doris Lee <dorisjunglinlee@gmail.com>
2020-01-02 20:56:35 -0800

    added hover div for vis div, still need to implement onclick function

--- react-jupyter-widget/src/currentView.tsx
+++ react-jupyter-widget/src/currentView.tsx
@@ -9,13 +9,19 @@ class CurrentViewComponent extends Component<currentViewProps,any> {
         super(props);
     }
     render() {
+        let selectedVis = function (vizLabel:string){
+            // console.log("selectedVis event:",event)
+            console.log(vizLabel)
+        }
         const chartData = {chartData: this.props.data}
         return (
             <div id="mainVizContainer">
                 <h2 id="mainVizTitle">Current View</h2>
                 <div id="mainVizInnerContainer">
-                    <VegaLite data={chartData} spec={this.props.currentViewSpec}
+                    <div className="vizContainer" onClick={()=>selectedVis("main")}>
+                        <VegaLite data={chartData} spec={this.props.currentViewSpec}
                               padding={{left: 30, top: 5, right: 5, bottom: 20}} />
+                    </div>
                 </div>
             </div>
         );
