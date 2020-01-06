b85a707f2ea0dfa2e97ddd067af02471636ca7aa
Doris Lee <dorisjunglinlee@gmail.com>
2020-01-05 20:27:57 -0800

    previous fix does not refresh, new bugfix that operate on copy

--- dataObj/dataObj.py
+++ dataObj/dataObj.py
@@ -21,7 +21,7 @@ class DataObj:
         self.mark = ""
         self.score = -1
         self.recommendation = {}
-        self.jsonWidget = {}
+        self.widgetJSON = {}
         self.compile()
 
     def __repr__(self):
@@ -77,35 +77,33 @@ class DataObj:
         return self.spec == []
 
     def toJSON(self,currentView=""):
-        if (self.jsonWidget=={}):
-            print ("here")
-            dobj_dict = {}
-            # Current View (if any)
-            if (type(self.compiled).__name__ == "DataObj"):
-                dobj_dict["currentView"] = self.compiled.renderVSpec()
-            if (type(self.compiled).__name__ == "DataObjCollection"):
-                # if the compiled object is a collection, see if we can remove the elements with "?" and generate a Current View
-                specifiedDobj = self.getVariableFieldsRemoved()
-                if (currentView!=""):
-                    dobj_dict["currentView"] = currentView.compiled.renderVSpec()
-                elif (specifiedDobj.isEmpty()):
-                    dobj_dict["currentView"] = {}
-                else:
-                    specifiedDobj.compile(enumerateCollection=False)
-                    dobj_dict["currentView"] = specifiedDobj.compiled.renderVSpec()
-            # Recommended Collection
-            dobj_dict["recommendations"] = []
-            if (self.recommendation != {}):
-                self.recommendation["vspec"] = []
-                for vis in self.recommendation["collection"].collection:
-                    chart = vis.renderVSpec()
-                    self.recommendation["vspec"].append(chart)
-                dobj_dict["recommendations"].append(self.recommendation)
-                # delete DataObjectCollection since not JSON serializable
-                if "collection" in dobj_dict["recommendations"][0]: del dobj_dict["recommendations"][0]["collection"]
-            return dobj_dict
-        else:
-            return self.jsonWidget
+        dobj_dict = {}
+        # Current View (if any)
+        if (type(self.compiled).__name__ == "DataObj"):
+            dobj_dict["currentView"] = self.compiled.renderVSpec()
+        if (type(self.compiled).__name__ == "DataObjCollection"):
+            # if the compiled object is a collection, see if we can remove the elements with "?" and generate a Current View
+            specifiedDobj = self.getVariableFieldsRemoved()
+            if (currentView!=""):
+                dobj_dict["currentView"] = currentView.compiled.renderVSpec()
+            elif (specifiedDobj.isEmpty()):
+                dobj_dict["currentView"] = {}
+            else:
+                specifiedDobj.compile(enumerateCollection=False)
+                dobj_dict["currentView"] = specifiedDobj.compiled.renderVSpec()
+        # Recommended Collection
+        dobj_dict["recommendations"] = []
+        import copy
+        recCopy= copy.copy(self.recommendation)
+        if (recCopy != {}):
+            recCopy["vspec"] = []
+            for vis in recCopy["collection"].collection:
+                chart = vis.renderVSpec()
+                recCopy["vspec"].append(chart)
+            dobj_dict["recommendations"].append(recCopy)
+            # delete DataObjectCollection since not JSON serializable
+            del dobj_dict["recommendations"][0]["collection"]
+        return dobj_dict
 
     def display(self, renderer="altair", currentView=""):
         # render this data object as: vis, columns, etc.?
@@ -117,11 +115,11 @@ class DataObj:
         # return widget
         # return chart
         import displayWidget
-        self.jsonWidget = self.toJSON(currentView=currentView)
+        widgetJSON = self.toJSON(currentView=currentView)
         widget = displayWidget.DisplayWidget(
             data=json.loads(self.dataset.df.to_json(orient='records')),
-            currentView=self.jsonWidget["currentView"],
-            recommendations=self.jsonWidget["recommendations"]
+            currentView=widgetJSON["currentView"],
+            recommendations=widgetJSON["recommendations"]
         )
         return widget
 
